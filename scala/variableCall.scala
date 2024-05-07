a1de9653d81ffe3057e4e26611bc8b4377b6a8a0
csaail <87662482+csaail@users.noreply.github.com>
2024-05-07 10:32:27 +0530

    Create variableCall.scala

---
+++ scala/variableCall.scala
@@ -0,0 +1,12 @@
+object Main 
+{
+  def main(args: Array[String])
+  {
+    var `name` = "Saail";
+    var _age = 21;
+    var Branch = "Computer Science";
+    println("Name:" +`name`);
+    println("Age:" +_age);
+    println("Branch:" +Branch);
+  }
+}
