8b3165e0ef2c5dde6151f9102087b1fd25eb9eb6
csaail <87662482+csaail@users.noreply.github.com>
2024-05-07 11:18:19 +0530

    Create collections.scala

---
+++ scala/collections.scala
@@ -0,0 +1,9 @@
+import scala.collection.immutable._
+object GFG { 
+	def main(args:Array[String]) 
+	{ 
+		val mylist = List("C", "C#", "Java", "Scala","PHP", "Ruby") 
+		println("The head of the list is:") 
+		println(mylist.head) 
+	} 
+}	 
