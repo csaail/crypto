ceea673ae9822e90dcf31c589bf05210353340bb
csaail <87662482+csaail@users.noreply.github.com>
2024-05-07 10:36:05 +0530

    Create IfElse.scala

---
+++ scala/IfElse.scala
@@ -0,0 +1,12 @@
+object ifElse 
+{ 
+    def main(args: Array[String]) { 
+        var a: Int = 650
+        if (a > 600)  
+        { 
+            println("Number greater than 600") 
+        } 
+        else{
+            println("Nmber less than 600")
+        }
+} 
