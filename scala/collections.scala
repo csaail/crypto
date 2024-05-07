686128fc5a11d207d25c698df8fbefec626fb47a
csaail <87662482+csaail@users.noreply.github.com>
2024-05-07 11:19:37 +0530

    Update collections.scala

--- scala/collections.scala
+++ scala/collections.scala
@@ -5,5 +5,8 @@ object GFG {
 		val mylist = List("C", "C#", "Java", "Scala","PHP", "Ruby") 
 		println("The head of the list is:") 
 		println(mylist.head) 
+		println("List is empty or not:") 
+        	println(mylist.isEmpty)
+		println("Reverse list:" + mylist.reverse) 
 	} 
 }	 
