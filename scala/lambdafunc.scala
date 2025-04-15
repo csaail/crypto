936019971dd58c02c6c3b6c7f651d465f045ecf9
csaail <saailchavan02@gmail.com>
2025-04-15 17:43:55 +0530

    Wipe all files and start fresh

--- scala/lambdafunc.scala
+++
@@ -1,8 +0,0 @@
-object lambda{ 
-  def main(args:Array[String]) { 
-  	val ex1 = (x:Int) => x + 2	 
-  	val ex2 = (x:Int, y:Int) => x * y 
-  	println(ex1(7)) 
-  	println(ex2(2, 3)) 
-  } 
-} 
--- scala/whileLoop.scala
+++
@@ -1,9 +0,0 @@
-object whileLoop{     
-  def main(args: Array[String]){
-    var x = 1;
-    while (x <= 4){
-      println("Value of x: " + x);
-      x = x + 1;
-    }
-  }
-}
